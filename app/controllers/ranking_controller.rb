21ea2c24225d4845b2b711f2cd209c339fb16eef
Jun Masuda <kibajun29@hotmail.co.jp>
2017-02-21 05:27:56 +0000

    ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,15 @@
 class RankingController < ApplicationController
   def have
+   Have.group(:item_id).order('count_item_id desc').count(:item_id)
+   @items = Item.finds(ids).sort_by do |element|
+     ids.index(element.id)
+   end
   end
 
   def want
+   Want.group(:item_id).order('count_item_id desc').count(:item_id)
+   @items = Item.finds(ids).sort_by do |element|
+     ids.index(element.id)
+   end
   end
 end
